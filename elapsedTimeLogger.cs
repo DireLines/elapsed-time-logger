1280537bb1e0ef87e2a5c13ef5bfdf9a026518ec
Nathaniel Saxe <NathanielSaxophone@gmail.com>
2023-01-21 11:56:48 -0500

    C#

---
+++ elapsedTimeLogger.cs
@@ -0,0 +1,13 @@
+using System;
+
+public static Action<string> elapsedTimeLogger(string logname) {
+    var watch = new System.Diagnostics.Stopwatch();
+    watch.Start();
+    return (message) => {
+        watch.Stop();
+        var elapsed = watch.ElapsedMilliseconds;
+        Console.WriteLine(String.Format("{0}: {1} took {2} millis",logname,message,elapsed));
+        watch.Reset();
+        watch.Start();
+    };
+}
